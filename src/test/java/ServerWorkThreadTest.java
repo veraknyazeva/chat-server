f0e8a43e0414ad9a75d703055e7556bc9c858cc1
Vera Knyazeva <verashibanova@yandex.ru>
2024-08-07 10:36:17 +0500

    second commit

--- src/test/java/ServerWorkThreadTest.java
+++ src/test/java/ServerWorkThreadTest.java
@@ -14,13 +14,6 @@ class ServerWorkThreadTest {
 
     @Test
     void requestNickname() {
-//        try {
-//            Method method = ServerWorkThread.class.getDeclaredMethod("requestNickname", null);
-//            method.setAccessible(true);
-//            assertEquals(method.invoke());
-//        }catch (Exception e){
-//            e.printStackTrace();
-//        }
 
     }
 
@@ -39,7 +32,6 @@ class ServerWorkThreadTest {
 
     @Test
     void getNickname() {
-        String nickname = "qwerty";
-        assertEquals("qwerty", nickname.toString());
+
     }
 }
